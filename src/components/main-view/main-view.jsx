fa7ff52122f5bef28d86050e9f39265c0d01b1d1
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-06 17:54:07 -0500

    Import MovieCard and MovieView components. Add selectedMovie state variable and set it as null. Add new state selectedMovie variable as flag and link MovieView. Link MovieCard to each iteration of movies state variable.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {MovieCard} from '../movie-card/movie-card';
+import {MovieView} from '../movie-view/movie-view';
 
 export class MainView extends React.Component{
   constructor(){ //creates component/class; good place to initialize values
@@ -8,17 +10,19 @@ export class MainView extends React.Component{
         {_id: 1, Title: 'Moulin Rouge!', Description: '', imagePath: ''},
         {_id: 2, Title: 'Moulin Rouge!', Description: '', imagePath: ''},
         {_id: 3, Title: 'Moulin Rouge!', Description: '', imagePath: ''}
-      ]
+      ],
+      selectedMovie: null
     }
   }
 
   render(){
-    const {movies} = this.state; //object destruction; equivalent to const movies = this.state.movies;
+    const {movies, selectedMovie} = this.state; //object destruction; equivalent to const movies = this.state.movies;
+    if(selectedMovie) return <MovieView movie = {selectedMovie}/>
     if(movies.length === 0) return <div className = "main-view">The list is empty!</div>; //curly braces required only for multiple statements, optional for single statement; else statement omitted
     return (
       <div className = "main-view">
         {/* map() loops through an array and calls a defined callback function on each element of an array, and returns an array that contains the results; in arrow function, return single statement does not require semicolon*/}
-        {movies.map(movie => <div key = {movie._id}>{movie.Title}</div>)} 
+        {movies.map(movie => <MovieCard key = {movie._id} movieData = {movie}/>)}
       </div>
     );
   }
