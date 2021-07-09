6a79df213af055a594638cdfc2478eb3777b533a
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-09 17:37:23 -0500

    Delete button and add click event listener to MovieCard component to change selectedMovie state

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -17,14 +17,14 @@ export class MainView extends React.Component{
 
   render(){
     const {movies, selectedMovie} = this.state; //object destruction; equivalent to const movies = this.state.movies;
-    if(selectedMovie) return <MovieView movie = {selectedMovie}/>
-    if(movies.length === 0) return <div className = "main-view">The list is empty!</div>; //curly braces required only for multiple statements, optional for single statement; else statement omitted
+    if(selectedMovie) return <MovieView movie = {selectedMovie}/>;
+    if(movies.length === 0) return <div className = "main-view">The list is empty!</div> //curly braces required only for multiple statements, optional for single statement; else statement omitted
     return (
       <div className = "main-view">
         {/* map() loops through an array and calls a defined callback function on each element of an array, and returns an array that contains the results; in arrow function, return single statement does not require semicolon*/}
-        <button onClick = {() => {alert('Nice!')}}>Click me!</button>
-        {movies.map(movie => <MovieCard key = {movie._id} movieData = {movie}/>)}
+        {movies.map(movie => <MovieCard key = {movie._id} movieData = {movie} onClick = {() => {this.state.selectedMovie = movie;}}/>)}
       </div>
     );
+    
   }
 }
