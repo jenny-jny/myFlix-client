0465c7bb11766916e6b253ba6e1c4860bca244fe
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-09 17:01:17 -0500

    Fix bug

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 export class MovieView extends React.Component{
   render(){
-    const {movieData} = this.props;
+    const {movie} = this.props;
     return (
       <div className = "movie-view">
         <div className = "movie-poster">
-          <img src = "movieData.imagePath"/>
+          <img src = {movie.imagePath}/>
         </div>
         <div className = "movie-title">
           <span className = "label">Title: </span>
-          <span className = "value">{movieData.Title}</span>
+          <span className = "value">{movie.Title}</span>
         </div>
         <div className = "movie-description">
           <span className = "label">Description: </span>
-          <span className = "value">{movieData.Description}</span>
+          <span className = "value">{movie.Description}</span>
         </div>
       </div>
     );
