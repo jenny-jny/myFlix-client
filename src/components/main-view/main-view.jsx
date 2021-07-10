3950aa6ba7f805d5406803e57448d58b24cf740c
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-09 19:11:13 -0500

    Refactor render method using ternary operators

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -21,12 +21,14 @@ export class MainView extends React.Component{
 
   render(){
     const {movies, selectedMovie} = this.state; //object destruction; equivalent to const movies = this.state.movies;
-    if(selectedMovie) return <MovieView movie = {selectedMovie}/>;
     if(movies.length === 0) return <div className = "main-view">The list is empty!</div> //curly braces required only for multiple statements, optional for single statement; else statement omitted
     return (
       <div className = "main-view">
+        {selectedMovie
+          ? <MovieView movie = {selectedMovie} onBackClick = {newSelectedMovie => this.setSelectedMovie(newSelectedMovie)}/>
+          : movies.map(movie => <MovieCard key = {movie._id} movieData = {movie} onMovieClick = {movie => this.setSelectedMovie(movie)}/>)
+        }
         {/* map() loops through an array and calls a defined callback function on each element of an array, and returns an array that contains the results; in arrow function, return single statement does not require semicolon*/}
-        {movies.map(movie => <MovieCard key = {movie._id} movieData = {movie} onMovieClick = {movie => this.setSelectedMovie(movie)}/>)}
       </div>
     );
     
