7825562c9cc0016feb29dcf12463b89a84578962
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-13 16:09:04 -0500

    Update movies array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,9 +7,9 @@ export class MainView extends React.Component{
     super(); //calls parent class React.Component
     this.state = { //refers to the MainView class instance created in memory
       movies: [
-        {_id: 1, Title: 'Moulin Rouge!', Description: '', imagePath: ''},
-        {_id: 2, Title: 'Moulin Rouge!', Description: '', imagePath: ''},
-        {_id: 3, Title: 'Moulin Rouge!', Description: '', imagePath: ''}
+        {imagePath: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Moulin_rouge_poster.jpg', _id: '60cc06b7ead29eb527ada014', Title: 'Moulin Rouge!', Description: 'Moulin Rouge! is a 2001 jukebox musical romantic drama film. It follows a young English poet, Christian, who falls in love with the star of the Moulin Rouge, cabaret actress and courtesan Satine. The film uses the musical setting of the Montmartre Quarter of Paris and is the final part of Luhrmann\'s \'Red Curtain Trilogy\'', Genre: 'Romance', Director: 'Baz Luhrmann'},
+        {imagePath: 'https://upload.wikimedia.org/wikipedia/en/c/c2/TheGreatGatsby2013Poster.jpg', _id: '60cc083eead29eb527ada016', Title: 'The Great Gatsby', Description: 'The Great Gatsby is a 2013 romantic drama film based on F. Scott Fitzgerald\'s 1925 novel. Jay-Z served as executive producer. The film follows the life and times of millionaire Jay Gatsby (DiCaprio) and his neighbor Nick Carraway (Maguire), who recounts his encounter with Gatsby at the height of the Roaring Twenties on Long Island.', Genre: 'Romance', Director: 'Baz Luhrmann'},
+        {imagePath: 'https://upload.wikimedia.org/wikipedia/en/1/19/Titanic_%28Official_Film_Poster%29.png', _id: '60cc0920ead29eb527ada017', Title: 'Titanic', Description: 'Titanic is a 1997 American epic romance and disaster film. Incorporating both historical and fictionalized aspects, it is based on accounts of the sinking of the RMS Titanic, and stars members of different social classes who fall in love aboard the ship during its ill-fated maiden voyage.', Genre: 'Romance', Director: 'James Cameron'}
       ],
       selectedMovie: null
     }
