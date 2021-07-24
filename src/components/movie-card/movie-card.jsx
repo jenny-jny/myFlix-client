aeadf3ff83fbe8d5227eb627fe8df83aa6153226
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-23 20:27:00 -0500

    Implement Button and Card from React-Bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component{
   render(){
     const {movieData, onMovieClick} = this.props;
-    return <div className = "movie-card" onClick = {() => onMovieClick(movieData)}>{movieData.Title}</div>
+    return (
+      <Card>
+        <Card.Img variant = "top" src = {movieData.ImagePath}/>
+        <Card.Body>
+          <Card.Title>{movieData.Title}</Card.Title>
+          <Card.Text>{movieData.Description}</Card.Text>
+          <Button onClick = {() => onMovieClick(movieData)} variant = "link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
