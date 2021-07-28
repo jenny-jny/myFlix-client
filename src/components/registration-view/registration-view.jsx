f5de23bba8b0bb301bfe439fbae0ece5e2f250a2
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-27 21:46:49 -0500

    Implement form validation and update send request to server

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -12,36 +12,56 @@ export function RegistrationView(props){
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
+  const [validated, setValidated] = useState(false);
 
   const handleSubmit = e => {
-    e.preventDefault();
+    const form = e.currentTarget;
+    if(form.checkValidity() === false){
+      e.preventDefault();
+    }
     console.log(username, password, email, birthday);
     //send a request to the server for authentication
-    //THEN call props.onLoggedIn(username)
-    props.onLoggedIn(username);
+    setValidated(true);
+    axios.post('http://jny-myflix.herokuapp.com/users/', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    }).then(response => {
+      const data = response.data;
+      //THEN call props.onLoggedIn(username)
+      props.onLoggedIn(data);
+    }).catch(() => {
+      console.log('no such user');
+    });
   };
+
   return (
     <Container>
       <Row className = "justify-content-md-center">
         <Col md = {8}>
-          <Form>
+          <Form noValidate validated = {validated} onSubmit = {handleSubmit}>
             <Form.Group controlId = "formUsername">
               <Form.Label>Username: </Form.Label>
-              <Form.Control type = "text" onChange = {e => setUsername(e.target.value)}/>
+              <Form.Control type = "text" onChange = {e => setUsername(e.target.value)} required/>
+              <Form.Control.Feedback type = "invalid">Please enter a username</Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId = "formPassword">
               <Form.Label>Password: </Form.Label>
-              <Form.Control type = "text" onChange = {e => setPassword(e.target.value)}/>
+              <Form.Control type = "password" onChange = {e => setPassword(e.target.value)} required/>
+              <Form.Control.Feedback type = "invalid">Please enter a password</Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId = "formEmail">
               <Form.Label>Email: </Form.Label>
-              <Form.Control type = "text" onChange = {e => setEmail(e.target.value)}/>
+              <Form.Control type = "email" onChange = {e => setEmail(e.target.value)} required/>
+              <Form.Control.Feedback type = "invalid">Please enter a email</Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId = "formBirthday">
-              <Form.Label>Username: </Form.Label>
-              <Form.Control type = "text" onChange = {e => setBirthday(e.target.value)}/>
+              <Form.Label>Birthday: </Form.Label>
+              <Form.Control type = "date" onChange = {e => setBirthday(e.target.value)} required/>
+              <Form.Control.Feedback type = "invalid">Please enter a birthday</Form.Control.Feedback>
             </Form.Group>
-            <Button variant = "primary" type = "submit" onClick = {handleSubmit}>Submit</Button>
+            <Button variant = "primary" type = "submit">Submit</Button>
           </Form>
         </Col>
       </Row>
