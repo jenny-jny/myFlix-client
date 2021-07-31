49dd105e396a5dfd33b06e1afc6a52274904a148
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-31 17:09:26 -0500

    Import Link local dependency and add Links to Genre and Director

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Container, Row, Col, Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 export class MovieView extends React.Component{
   render(){
@@ -21,12 +22,18 @@ export class MovieView extends React.Component{
               <span className = "value">{movie.Description}</span>
             </div>
             <div className = "movie-genre">
-              <span className = "label">Genre: </span>
-              <span className = "value">{movie.Genre.Name}</span>
+              {/* <span className = "label">Genre: </span>
+              <span className = "value">{movie.Genre.Name}</span> */}
+              <Link to = {`genres/${movie.Genre.Name}`}>
+                <Button variant = "link">Genre</Button>
+              </Link>
             </div>
             <div className = "movie-director">
-              <span className = "label">Director: </span>
-              <span className = "value">{movie.Director.Name}</span>
+              {/* <span className = "label">Director: </span>
+              <span className = "value">{movie.Director.Name}</span> */}
+              <Link to = {`directors/${movie.Director.Name}`}>
+                <Button variant = "link">Director</Button>
+              </Link>
             </div>
             <Button onClick = {() => onBackClick(null)}>Back</Button>
           </Col>
