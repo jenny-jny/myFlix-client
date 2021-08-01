75e2004e935e02645d4fcf3b867fa25557e5c962
jenny-jny <jenny.yu.0414@gmail.com>
2021-08-01 15:47:01 -0500

    Initial commit

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import {Container, Row, Col, Button} from 'react-bootstrap';
+
 
 export function GenreView(props){
-
+  return (
+    <Container>
+      <Row className = "justify-content-md-center">
+        <Col md = {8}>{props.genre.Name}</Col>
+      </Row>
+      <Row>
+        <Col md = {8}>{props.genre.Description}</Col>
+      </Row>
+    </Container>
+  );
 }
+
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
