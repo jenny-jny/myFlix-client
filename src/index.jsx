070308151490580194a181275f16ae9c3e46e240
jenny-jny <jenny.yu.0414@gmail.com>
2021-08-25 15:59:53 -0500

    Import devToolsEnhancer development dependency and create store there

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import moviesApp from './reducers/reducers';
+import {devToolsEnhancer} from 'redux-devtools-extension';
 
+import moviesApp from './reducers/reducers';
 import {MainView} from './components/main-view/main-view';
 
 //import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
 
-const store = createStore(moviesApp);
+const store = createStore(moviesApp, devToolsEnhancer());
 
 //main component (will eventually use all the others)
 class MyFlixApplication extends React.Component{
