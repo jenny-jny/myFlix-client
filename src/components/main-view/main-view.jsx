b056bf9809ca712791871ce26d3fbd3274267ad0
jenny-jny <jenny.yu.0414@gmail.com>
2021-08-29 20:12:06 -0500

    Fix bug

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -89,7 +89,7 @@ class MainView extends React.Component{
                   </Link> 
                 </>
               );
-              return <Redirect to = '/movies'/>;
+              return <Redirect to = '/movies1'/>;
               }}/>
             <Route exact path = "/register" render = {() => { 
               //If there is no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView
